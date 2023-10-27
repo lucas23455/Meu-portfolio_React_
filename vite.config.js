ce10ce1a92115b76f46f9a29bd77c86dc96a68d6
lucas230455 <lucaspassos2002@gmail.com>
2023-10-27 09:55:53 -0300

    configuraçoes de deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Meu_Portifolio_React-Sass"
+  base: "/Meu-portfolio_React_"
 })
